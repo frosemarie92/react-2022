04dd512b25d765989ba5561a9abb895d506779a7
frosemarie92 <frosemarie92@gmail.com>
2022-07-17 15:35:01 -0700

    creating react components
    Weather

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
-
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Rosemarie Feliciano and is{" "}
-        <a href="https://github.com/frosemarie92/react-2022" target="_blank">
-          open-sourced on github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Rosemarie Feliciano and is{" "}
+          <a href="https://github.com/frosemarie92/react-2022" target="_blank">
+            open-sourced on github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
