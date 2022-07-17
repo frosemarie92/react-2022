f6881903e8043f9172f3e368fd3a7d8ae8ad7925
frosemarie92 <frosemarie92@gmail.com>
2022-07-17 14:59:53 -0700

    add github url

--- src/App.js
+++ src/App.js
@@ -3,5 +3,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Rosemarie Feliciano and is{" "}
+        <a href="https://github.com/frosemarie92/react-2022" target="_blank">
+          open-sourced on github
+        </a>
+      </footer>
+    </div>
+  );
 }
